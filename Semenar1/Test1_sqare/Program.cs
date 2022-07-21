478346a5641fca1132ac414183c9741c459b8bca
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-21 20:53:53 +0300

    1 var

--- Semenar1/Test1_sqare/Program.cs
+++ Semenar1/Test1_sqare/Program.cs
@@ -2,6 +2,15 @@
 
 Console.WriteLine("Enter number: ");
 string userenter = Console.ReadLine();
-number = int.Parse(userenter);
 
-Console.Write($"Sqare {number} = {number * number}");
+bool enter = int.TryParse(userenter, out number); //Перевод username в int и запись в number
+//если получится, то bool = true
+if (enter)
+{
+    Console.Write($"Sqare {number} = {number * number}");
+}
+//если не получится перевести, то bool = false
+if (!enter)
+{
+    Console.Write("Wrong input");
+}
