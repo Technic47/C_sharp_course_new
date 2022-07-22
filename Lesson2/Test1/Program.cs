45ad75b8f9e636a1be1bdc6a174ecdaf3b13e725
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-22 12:38:52 +0300

    done

--- Lesson2/Test1/Program.cs
+++ Lesson2/Test1/Program.cs
@@ -12,10 +12,13 @@ int c1 = 39;
 int a2 = 12;
 int b2 = 23;
 int c2 = 33;
-int a3 = 13;
+int a3 = 199;
 int b3 = 23;
 int c3 = 33;
 
 int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3);
 
-Console.Write(max1);
+Console.Write(max);
