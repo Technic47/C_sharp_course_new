1cdcd707d660c04b38efff3e6507e7cb1bbbeaaf
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-22 12:54:01 +0300

    test2 added

---
+++ Lesson2/Test2/Program.cs
@@ -0,0 +1,12 @@
+﻿
+int Max(int arg1, int arg2, int arg3) //Function Max created
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int[] array = { 11, 21, 31, 41, 51, 61, 12, 13, 15 }; //Array created
+array[0] = 12;
+Console.WriteLine(array[0]);
