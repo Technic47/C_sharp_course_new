1b2496dbcc7a7210545dbe5bc2454e484a804f10
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-22 12:59:31 +0300

    variant1

--- Lesson2/Test2/Program.cs
+++ Lesson2/Test2/Program.cs
@@ -8,5 +8,12 @@ int Max(int arg1, int arg2, int arg3) //Function Max created
 }
 
 int[] array = { 11, 21, 31, 41, 51, 61, 12, 13, 15 }; //Array created
-array[0] = 12;
-Console.WriteLine(array[0]);
+//array[0] = 12; //Rename index[0]
+
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+Console.WriteLine(max);
