fcc239379ef1d3210aacc058ff8717c248cbba7d
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-22 21:03:19 +0300

    Test 1 func done

--- Semenar2/Test1/Program.cs
+++ Semenar2/Test1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 0;
+
+a = int.Parse(Console.ReadLine());
+
+int Sqr(int temp)
+{
+    int t = temp * temp;
+    return t;
+    
+}
+int b = 0;
+b = Sqr(a);
+Console.WriteLine($"Sqr = {b}");
