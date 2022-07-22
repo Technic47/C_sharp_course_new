328a5b53e4abd60fa91dbaca21534ec2a264314b
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-22 22:14:10 +0300

    Test 2 added

---
+++ Semenar2/Test2/Program.cs
@@ -0,0 +1,10 @@
+﻿int[] array = new int[9]; // array with index=8 (9 elements)
+array[5] = 8;
+//for (int i = 0; i < 10; i++)
+//{
+//    Console.WriteLine($"array[{i}] = {array[i]}\n");
+//    
+//}
+array[3]=4;
+array[4]=5;
+Console.WriteLine($"{array[3]} {array[4]}");
