3032b87a96c4696fc3637c7441c00cbac13bb603
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-25 22:13:47 +0300

    Semenar3 done

---
+++ Semenar3/Test2/Program.cs
@@ -0,0 +1,8 @@
+﻿//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+int num = new Random().Next(100, 1000);
+
+int a1 = num / 100;
+int a2 = num % 10;
+Console.WriteLine(num);
+
+Console.WriteLine((a1*10) + a2);
---
+++ Semenar3/Test3/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+int a = 0;
+a = int.Parse(Console.ReadLine());
+
+if (((a % 7) == 0) && ((a % 23) == 0))
+{
+    Console.WriteLine("True");
+}
+else 
+{
+    Console.WriteLine("False");
+}
