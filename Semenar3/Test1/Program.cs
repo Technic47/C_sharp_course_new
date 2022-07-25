2cf81fdce81289a98bb31e93a0a453f2aa05ca90
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-25 20:57:14 +0300

    Test1

---
+++ Semenar3/Test1/Program.cs
@@ -0,0 +1,15 @@
+﻿int num = new Random().Next(10, 100);
+
+int a1 = num / 10; // целая часть - первое число
+int a2 = num % 10; // дробная - второе число
+
+Console.Write(num);
+if (a1 > a2)
+{
+    Console.Write($" -> {a1}");
+}
+else
+{
+    Console.Write($" -> {a2}");
+
+}
