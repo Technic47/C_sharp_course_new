f1c1e613ef6eb3c13ceff681b1c3d6430cf3bc6a
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-26 14:13:24 +0300

    Test4 done

---
+++ Lesson3/Test4/Program.cs
@@ -0,0 +1,32 @@
+﻿//Упорядочить массив методом выбора
+int[] arr = {1,2,5,4,6,4,5,9,6};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPossition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPossition]) minPossition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPossition];
+        array[minPossition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
