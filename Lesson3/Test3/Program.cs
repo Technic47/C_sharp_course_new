a6333935a9e94cdc4c50c29ec51fbf4dc5496fd7
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-26 13:54:15 +0300

    Test3 done

---
+++ Lesson3/Test3/Program.cs
@@ -0,0 +1,23 @@
+﻿string text = "I checked this message for stupidity"
++" and found out that"
++" I am not abele to read.";
+
+// string 5 = "text" => "0123" как массив
+//s[3] => t
+
+string Replace(string text, char oldW, char newW)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for(int i = 0; i < length; i++)
+    {
+        if (text[i] == oldW) result = result + $"{newW}";
+        else result = result + $"{text[i]}";
+    }
+
+    return result;
+}
+
+string newText = Replace(text, ' ', '|');
+Console.WriteLine(newText);
