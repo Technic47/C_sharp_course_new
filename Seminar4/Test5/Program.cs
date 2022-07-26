39327938e9d66bc33487ea0a969d11967b928ab3
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-26 22:33:32 +0300

    Test5 done

--- Seminar4/Test5/Program.cs
+++ Seminar4/Test5/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*Напишите программу, 
+которая принимает на вход число N
+ и выдаёт произведение чисел от 1 до N.
+
+4  -> 24
+5  -> 120
+*/
+
+int A = int.Parse(Console.ReadLine()!);
+
+long Sum(int A)
+{
+    long sum = 1;
+    for(int i = 1; i <= A; i++)
+    {
+        sum = sum * i;
+    }
+    return sum;
+}
+
+Console.WriteLine(Sum(A));
+
+
+//заполнить массив из восьми элементов 0 и 1 рандомно
+/*Console.WriteLine("Enter array size:");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+int[] array = new int[A];
+
+int i = 0;
+while ( i < array.Length)
+{
+    array[i] = new Random().Next(0,2);
+    Console.Write($"{array[i]} ");
+    i++;
+}
+*/
