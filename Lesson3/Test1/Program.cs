cac1cce8c985d81de2149bfd4907c6ea8445ac9f
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-26 13:32:15 +0300

    Test1 added

---
+++ Lesson3/Test1/Program.cs
@@ -0,0 +1,12 @@
+﻿string Method4 ( int count, string text)
+{
+    int i = 0;
+    string result = String.Empty;
+    while (i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+Console.WriteLine(Method4(5, "Text"));
