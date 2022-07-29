fbb810e51c726e76ea9c266a0df86d9de79cdbc3
Pavel Kuznetsov <Technic47@gmail.com>
2022-07-29 13:27:06 +0300

    Lesso4 added

---
+++ Lesson4 double arrays/Test1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+string[,] table = new string[2, 5];
+//table[0, 0] table[0, 1] table[0, 2] table[0, 3]
+//table[1, 0] table[1, 1] table[1, 2] table[1, 3]
+
+table[1, 2] = "Text"; // обращение к массиву
+
+for (int rows = 0; rows < 2; rows++)
+{
+    for (int columns = 0; columns < 5; columns++)
+    {
+        Console.WriteLine($"--{table[rows, columns]}-");
+    }
+}
+*/
+int[,] matrix = new int[3, 4]; // вывод в виде матрицы
+
+for (int i = 0; i < matrix.GetLength(0); i++) //строки
+{
+    for (int j = 0; j < matrix.GetLength(1); j++) //столбцы
+    {
+        Console.Write($"{matrix[i, j]} ");
+    }
+Console.WriteLine();
+}
---
+++ Lesson4 double arrays/Test2/Program.cs
@@ -0,0 +1,30 @@
+﻿int[,] matrix = new int[3, 4];
+
+void PrintArray(int[,] mart) // create and print
+{
+    for (int i = 0; i < mart.GetLength(0); i++) //строки
+    {
+        for (int j = 0; j < mart.GetLength(1); j++) //столбцы
+        {
+            Console.Write($"{mart[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] mart) // Fill with random
+{
+    for (int i = 0; i < mart.GetLength(0); i++) //строки(0)
+    {
+        for (int j = 0; j < mart.GetLength(1); j++) //столбцы(1)
+        {
+            mart[i,j] = new Random().Next(0,10);
+            Console.Write($"{mart[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+PrintArray(matrix);
+Console.WriteLine();
+FillArray(matrix);
