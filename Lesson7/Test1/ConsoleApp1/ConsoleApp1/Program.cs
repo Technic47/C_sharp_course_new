60cf6c6b5b86f342cb7926a38d5181987a812fcd
Pavel Kuznetsov <Technic47@gmail.com>
2022-08-04 20:09:03 +0300

    Seminar9 added

---
+++ Lesson7/Test1/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿class Program
+{
+    static public void Main(string[] args)
+    {
+        if (args.Length >=1)
+        {
+            if (args[0] == "")
+            Console.WriteLine($"Hello, {args[0].Replace("-", "")}!");
+         
+        }
+        
+        Console.ReadKey();
+    }
+}
+
